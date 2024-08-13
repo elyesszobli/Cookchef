483cf4a4b7a798a9323eb336547333aa3fa65f52
elyesszobli <elyesszobli@gmail.com>
2024-08-13 23:40:57 +0200

    Mise en place du Header et du Footer

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,5 +1,5 @@
 function Content() {
-  return <h1 className="flex-fill b3">Content</h1>;
+  return <h1 className="flex-fill b3"></h1>;
 }
 
 export default Content;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,21 @@
+import styles from "./Header.module.scss";
+import cookchef from "../assets/images/cookchef.png";
+
 function Header() {
-  return <h1 className="b1">Header</h1>;
+  return (
+    <header className={`${styles.header} d-flex flex-row align-items-center`}>
+      <i className="fa-solid fa-bars mr-15"></i>
+      <div className="flex-fill">
+        <img src={cookchef} alt="logo cookchef" />
+      </div>
+      <ul>
+        <button className="mr-5 btn btn-reverse-primary">
+          <i className="fa-solid fa-basket-shopping mr-5"></i>
+          panier</button>
+        <button className="btn btn-primary">connexion</button>
+      </ul>
+    </header>
+  );
 }
 
 export default Header;
